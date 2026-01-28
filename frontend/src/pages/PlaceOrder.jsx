bdbe4734b39469f9141c0010af73b34f1a2ef5b2
Vaishali <rawatvaishali254@gmail.com>
2026-01-28 20:54:43 +0530

    created place order page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -5,26 +5,26 @@ const PlaceOrder = () => {
   const {getTotalCartAmount} = useContext(StoreContext);
 
   return (
-    <form>
-      <div>
-        <p>Delivery Information</p>
-        <div>
-          <input type="text" placeholder='First name'/>
-          <input type="text" placeholder='Last name'/>
+    <form className='flex items-start justify-between gap-12.5 mt-25 font-[outfit]'>
+      <div className='w-full'>
+        <p className='text-[30px] font-bold mb-12.5'>Delivery Information</p>
+        <div className='flex gap-2.5'>
+          <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='First name'/>
+          <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='Last name'/>
         </div>
-        <input type="email" placeholder='Email address'/>
-        <input type="text" placeholder='Street'/>
-        <div>
-          <input type="text" placeholder='City'/>
-          <input type="text" placeholder='State'/>
+        <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="email" placeholder='Email address'/>
+        <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='Street'/>
+        <div className='flex gap-2.5'>
+          <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='City'/>
+          <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='State'/>
         </div>
-        <div>
-          <input type="text" placeholder='Zip code'/>
-          <input type="text" placeholder='Country'/>
+        <div className='flex gap-2.5'>
+          <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='Zip code'/>
+          <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='Country'/>
         </div>
-        <input type="text" placeholder='Phone'/>
+        <input className='mb-3.75 w-full p-2.5 border border-[#c5c5c5] rounded-sm outline-[#B87C4C]' type="text" placeholder='Phone'/>
       </div>
-      <div>
+      <div className='w-full'>
         <div className='flex-1'>
           <h2 className='font-bold text-2xl mb-4'>Cart Totals</h2>
 
@@ -49,7 +49,7 @@ const PlaceOrder = () => {
             </div>
           </div>
 
-          <button className='mt-6 w-full md:w-60 text-white bg-[#B87C4C] hover:bg-[#87552d] py-3 rounded-sm'>
+          <button className='mt-7.5 w-full md:w-60 text-white bg-[#B87C4C] hover:bg-[#87552d] py-3 rounded-sm'>
             PROCEED TO PAYMENT
           </button>
         </div>
