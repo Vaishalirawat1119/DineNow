b08a6156894b51d42b2ec294e0e416d73d05e7c4
Vaishali <rawatvaishali254@gmail.com>
2026-02-11 20:29:13 +0530

    created user register api

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config.js"
 
 // app config
 const app = express();
@@ -17,6 +19,7 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/image", express.static("uploads"))
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {          //get() - HTTP method that request data from the server
     res.send("API Working");
