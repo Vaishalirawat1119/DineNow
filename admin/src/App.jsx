6ea07f3361d4311b8d6420a956cdb0d10e331c9f
Vaishali <rawatvaishali254@gmail.com>
2026-02-11 14:14:46 +0530

    Add component is completed and also responsive for all screen

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,12 @@ import { Route, Routes } from 'react-router-dom'
 import Add from './pages/add'
 import List from './pages/List'
 import Orders from './pages/Orders'
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <div className='min-h-screen'>
+      <ToastContainer/>
       <Navbar/>
       <hr className='h-px border-[none]' />
       <div className='flex'>
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,29 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from "axios";
+import { toast } from 'react-toastify';
 
 const Add = () => {
+    const url = "http://localhost:4000";
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name : "",
+        description : "",
+        price : "",
+        category : "Salad"
+    });
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({...data, [name] : value}))
+    }
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("price", Number(data.price));
+        formData.append("category", data.category);
+        formData.append("image", image);
+
+        const response = await axios.post(`${url}/api/food/add`, formData);
+
+        if(response.data.success){
+            setData({
+                name : "",
+                description : "",
+                price : "",
+                category : "Salad"
+            })
+            setImage(false);
+            toast.success(response.data.message)
+        }
+        else{
+            toast.error(response.data.message);
+        }
+    }
+
   return (
-    <div className='w-[70%] ml-[max(5vw,25px)] mt-12.5 text-[#6d6d6d] text-[16px]'>
-        <form className='gap-5 flex-col-base'>
+    <div className='w-full md:w-[85%] lg:w-[70%] px-4 md:px-0 ml-0 md:ml-[max(5vw,25px)] mt-8 md:mt-12.5 text-[#6d6d6d] text-[14px] md:text-[16px]'>
+        <form className='gap-5 flex-col-base' onSubmit={onSubmitHandler}>
+
             <div className='flex-col-base'>
                 <p>Upload Image</p>
                 <label htmlFor="image">
-                    <img className='w-30' src={assets.upload_area} alt="" />
+                    <img className='w-24 md:w-30 cursor-pointer' src={image ? URL.createObjectURL(image) : assets.upload_area} alt="" />
                 </label>
-                <input className='input-border' type="file" id='image' hidden required/>
+                <input 
+                    onChange={(e) => setImage(e.target.files[0])} 
+                    className='input-border' 
+                    type="file" 
+                    id='image' 
+                    hidden 
+                    required
+                />
             </div>
-            <div className='flex-col-base product-name-desc-style'>
+
+            <div className='flex-col-base product-name-desc-style w-full md:w-[max(40%,280px)]'>
                 <p>Product Name</p>
-                <input className='input-border' type="text" name="name" placeholder='Type here'/>
+                <input onChange={onChangeHandler} value={data.name} className='input-border w-full' type="text" name="name" placeholder='Type here' required/>
             </div>
-            <div className='flex-col-base product-name-desc-style'>
+
+            <div className='flex-col-base product-name-desc-style w-full md:w-[max(40%,280px)]'>
                 <p>Product Description</p>
-                <textarea className='input-border' name="description" rows="6" placeholder='Write content here' required></textarea>
+                <textarea onChange={onChangeHandler} value={data.description} className='input-border w-full' name="description" rows="6" placeholder='Write content here' required></textarea>
             </div>
-            <div className='flex gap-7.5'>
-                <div className='flex-col-base'>
+
+            <div className='flex gap-5 md:gap-7.5'>
+                <div className='flex-col-base w-full md:w-auto'>
                     <p>Product Category</p>
-                    <select className='input-border' name="category">
+                    <select onChange={onChangeHandler} value={data.category} className='input-border w-full' name="category">
                         <option value="Salad">Salad</option>
                         <option value="Rolls">Rolls</option>
                         <option value="Deserts">Deserts</option>
@@ -34,12 +88,17 @@ const Add = () => {
                         <option value="Noodles">Noodles</option>
                     </select>
                 </div>
-                <div className='flex-col-base'>
+
+                <div className='flex-col-base w-full md:w-auto'>
                     <p>Product Price</p>
-                    <input className='input-border' type="Number" name="price" placeholder='$20'/>
+                    <input onChange={onChangeHandler} value={data.price} className='input-border w-full' type="number" name="price" placeholder='$20'/>
                 </div>
             </div>
-            <button className='input-border max-w-30 p-2.5 bg-black text-white cursor-pointer' type='submit'>ADD</button>
+
+            <button className='input-border w-full md:w-auto max-w-30 p-2.5 bg-black text-white cursor-pointer' type='submit'>
+                ADD
+            </button>
+
         </form>
     </div>
   )
