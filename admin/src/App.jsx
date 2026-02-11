863ca874da75fccb2a011913b082bd1d5f7d927d
Vaishali <rawatvaishali254@gmail.com>
2026-02-11 19:54:58 +0530

    create url in one file

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,6 +8,7 @@ import Orders from './pages/Orders'
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
+  const url = "http://localhost:4000";
   return (
     <div className='min-h-screen'>
       <ToastContainer/>
@@ -16,9 +17,9 @@ const App = () => {
       <div className='flex'>
         <Sidebar/>
         <Routes>
-          <Route path='/add' element={<Add/>}/>
-          <Route path='/list' element={<List/>}/>
-          <Route path='/orders' element={<Orders/>}/>
+          <Route path='/add' element={<Add url={url}/>}/>
+          <Route path='/list' element={<List url={url}/>}/>
+          <Route path='/orders' element={<Orders url={url}/>}/>
         </Routes>
       </div>
     </div>
